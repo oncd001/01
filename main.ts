29d061c0c31bdf37ce78260aa8878a767bc6b972
oncd001 <82135658+oncd001@users.noreply.github.com>
2021-04-09 00:47:52 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,12 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("start")
     걸음수 = 0
     while (!(input.buttonIsPressed(Button.B))) {
-        for (let index = 0; index < 3; index++) {
-            basic.showIcon(IconNames.Giraffe)
-            basic.pause(100)
-            basic.clearScreen()
-            basic.pause(100)
-        }
+    	
     }
 })
 input.onGesture(Gesture.Shake, function () {
@@ -16,23 +11,17 @@ input.onGesture(Gesture.Shake, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(걸음수)
     while (!(input.buttonIsPressed(Button.A))) {
-        basic.showIcon(IconNames.Heart)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.showIcon(IconNames.Heart)
+    	
     }
 })
 let 걸음수 = 0
-basic.showNumber(0)
-basic.pause(2000)
-basic.showString("Hello!")
+basic.showString("Hi")
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.pause(100)
+}
 basic.forever(function () {
     if (10 < 걸음수) {
         basic.showIcon(IconNames.Heart)
